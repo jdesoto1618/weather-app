7551bf040eaeb4041e75f4f3f8a58a8e2f6a1af3
Joe DeSoto <modulusman@gmail.com>
2021-07-03 10:57:30 -0700

    Adds styles to app component such as setting background images and adding paddings.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Search from './components/search/Search';
+
 const api = {
   key: "48dd64b94b33bf63268037d5037ac034",
   base: "api.openweathermap.org/data/2.5/"
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,8 +1,15 @@
+import './search.css';
 
 const Search = ({ apiInfo }) => {
   return (
     <div className='conponent-container search-container'>
-
+      <div className='search-box'>
+        <input
+          type='text'
+          className='search-bar'
+          placeholder='Search...'
+        />
+      </div>
     </div>
   );
 }
