90086241fbe8aff6ce4fcb20ab68350780ac9a0f
Joe DeSoto <modulusman@gmail.com>
2021-07-03 10:23:43 -0700

    Adds weather api to app component. Also adds free stock images.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+const api = {
+  key: "48dd64b94b33bf63268037d5037ac034",
+  base: "api.openweathermap.org/data/2.5/"
+}
 
-function App() {
+const App = () => {
   return (
     <div className="App">
     </div>
