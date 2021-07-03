826a46255654687cd83a9d5c0e66c1875c0aae5f
Joe DeSoto <modulusman@gmail.com>
2021-07-03 11:51:27 -0700

    Adds dynamic date function that generates a date in a specific format and adds it to the page.

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,6 +1,18 @@
 import './location.css'
 
 const Location = () => {
+  const dateBuilder = (date) => {
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let day = days[date.getDay()];
+    let theDate = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+
+    return `${day} ${theDate} ${month} ${year}`;
+  }
+
   return (
     <div className='component-container location-container'>
       <div className='location-box'>
